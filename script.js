719ab03fb08ca3888e700d705105caa483dda74a
ZakirTqvi <husainitaqvi4@gmail.com>
2024-11-14 21:28:11 +0530

    new files

---
+++ script.js
@@ -0,0 +1,28 @@
+let input = document.getElementById('userInput')
+let buttons = document.querySelectorAll('button')
+
+let string = ""
+let arr = Array.from(buttons);
+
+// console.log(arr)
+
+arr.forEach( (button) =>{
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC') {
+            string = ''
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL') {
+            string = string.slice(0,-1)
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
